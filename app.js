1613252e9fcb9a0644315c459f4f57a04e78ebdc
Andres: Abreu <94572199+andres-abreu@users.noreply.github.com>
2022-02-15 14:00:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
-};
+const message = 'Hello Node!';
 
-printProfileData('profileDataArgs');
+if (true === true) {
+  const message = 'Hello ES6';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
+
+console.log(message);
+console.log(sum);
+
